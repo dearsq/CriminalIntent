858d84ab21f0afbeb02940277aaf093f2f86abd0
章扬 <foreveryounix@gmail.com>
2018-11-13 10:00:27 +0800

    public abstract class SingleFragmentActivity

--- app/src/main/java/com/iyounix/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/iyounix/android/criminalintent/CrimeLab.java
@@ -2,8 +2,14 @@ package com.iyounix.android.criminalintent;
 
 import android.content.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class CrimeLab {
     private static CrimeLab sCrimeLab;  // s前缀表示静态变量约定
+
+    private List<Crime> mCrimes;
 
     public static CrimeLab get(Context context) {
         if (sCrimeLab == null){
@@ -12,8 +18,24 @@ public class CrimeLab {
         return sCrimeLab;
     }
 
+    public Crime getCrime(UUID id) {
+        for(Crime crime: mCrimes) {
+            if (crime.getId().equals(id)) {
+                return crime;
+            }
+        }
+        return null;
+    }
+
     // 私有构造方法
     private CrimeLab(Context context){
-
+        // 由于 private List<Crime> mCrimes; 所以编译器推断出 ArrayList 可存放 Crime 对象
+        mCrimes = new ArrayList<>();  // 使用常规Java数组存储列表元素
+        for(int i = 0 ; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime #"+i);
+            crime.setSolved(i % 2 == 0); //设置偶数没有被解决,奇数被解决
+            mCrimes.add(crime);
+        }
     }
 }
--- app/src/main/java/com/iyounix/android/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/iyounix/android/criminalintent/SingleFragmentActivity.java
@@ -1,25 +1,23 @@
 package com.iyounix.android.criminalintent;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class CrimeActivity extends AppCompatActivity {
+//抽象类
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    //抽象方法
+    protected abstract Fragment createFragment();
 
-    /**
-     * 保护方法
-     * @param savedInstanceState
-     */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
 
-        // 获取 Activity 的 FragmentManager // FragmentManager 管理着 fragment事务会退栈
         FragmentManager fm = getSupportFragmentManager();
 
-        // 将我们的 fragment 给 fm 管理
         Fragment fragment = fm.findFragmentById(R.id.fragment_container); //fragment 在 -> 销毁时 FM 会优先获取保存的队列, 然后重建
         if (fragment == null) { // fragment 不在, 启动新 fragment 事务
             fragment = new CrimeFragment();
